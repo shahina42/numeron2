66220d1a820345f67fa8c6ce96ede3ef62891664
shahina42 <141030593+shahina42@users.noreply.github.com>
2023-11-23 15:56:55 +0530

    first commit

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 // Iteration 1: Making the play button in the index.html functional.
+var playbtn = document.getElementById("play-button")
+
 // Description: When the play button is clicked the game.html page should be opened
+
+playbtn.addEventListener("click",function(){
+    window.location.href="game.html"
+})
